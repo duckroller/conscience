068ae8df4f4338cf6daeeed5157d57c1fa15c1fe
Minh Vu <mdvu15@earlham.edu>
2016-11-20 09:02:14 -0600

    almost final

--- make_call.py
+++ make_call.py
@@ -1,4 +1,3 @@
-# Download the library from twilio.com/docs/libraries
 from twilio.rest import TwilioRestClient
 
 # Get these credentials from http://twilio.com/user/account
@@ -6,8 +5,9 @@ account_sid = "AC90271a88fb3800a9d79b9323595beadf"
 auth_token = "64d8aa95c86d2ec9af232a3e8e6e64b4"
 client = TwilioRestClient(account_sid, auth_token)
 
-# Make the call
-call = client.calls.create(to="+17652772139",  # Any phone number
+call = client.calls.create(to="+13176941608",  # Any phone number
                            from_="+13475806502", # Must be a valid Twilio number
-                           url="https://c03a5d1f.ngrok.io/")
+                           ApplicationSid="AP9fe10acd6ce64163facc3f7a81ccbef3",
+                           url ="https://8e9605d6.ngrok.io"
+                           )
 print(call.sid)
--- sms_run.py
+++ sms_run.py
@@ -2,6 +2,8 @@ from flask import Flask, request, redirect
 import twilio.twiml
 import requests
 import json
+#import sys
+#import os
 
 app = Flask(__name__)
 
@@ -11,29 +13,67 @@ def barkCheck(messageText):
         'X-Token-Auth': 'rn4t4uzMufZYNtteRDS9hvZK',
     }
 
-    data = "{ \"message\": \"" + messageText + "\" }"
+    data = "{ \"message\": \"" + str(messageText) + "\" }"
 
     return requests.post('https://partner.bark.us/api/v1/messages', headers=headers, data=data)
 
+def clarifaiCheck(inputURL):
+    headers = {
+       'Authorization': 'Bearer MVCiDHu5zOcYsBRkvzgFzVOtG8elYA',
+   }
+
+    urlToCheck = 'https://api.clarifai.com/v1/tag/?model=nsfw-v1.0&url=' + str(inputURL)
+
+    return requests.get(urlToCheck, headers=headers)
+
+#def getNumber():
+#    number = request.values.get('From', None)
+#    return number
+
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
-
+    #makeCall = False
     content = request.values.get('Body', None)
-    result = json.loads(barkCheck(content).text)
-    if result["success"] == False:
-        message = "Dude! That's not even a valid message"
-    else:
-        if result["abusive"] == False:
-            message = "Not abusive at all, you are good to go!"
+    media = request.values.get('NumMedia', None)
+    resultText = json.loads(barkCheck(content).text)
+    if int(media) != 0:
+        image = request.form['MediaUrl0']
+        resultImage = json.loads(clarifaiCheck(image).text)['results'][0]['result']['tag']['classes'][0]
+        probability = json.loads(clarifaiCheck(image).text)['results'][0]['result']['tag']['probs'][0]
+        if resultText["success"] == False:
+            if resultImage == 'nsfw' and probability >= .85:
+    #            makeCall = True
+                message = "This is your conscience speaking, you know you shouldn't have sent that photo"
+            else:
+                message = "Your message doesn't make sense, but you're fine! "
         else:
-            message = "Hey stop! I see what you did there!"
+            if resultText["abusive"] == False and resultImage == 'sfw':
+                message = "Not abusive at all, you are good to go! "
+            else:
+    #            makeCall = True
+                message = "Hey stop! I see what you did there! "
+    elif int(media) == 0:
+        if resultText["success"] == False:
+            message = "Your message doesn't make sense, but you're fine! "
+        else:
+            if resultText["abusive"] == False:
+                message = "Not abusive at all, you are good to go! "
+            else:
+    #            makeCall = True
+                message = "Hey stop! I see what you did there! "
+    else:
+        message = "This is your conscience speaking!"
 
     resp = twilio.twiml.Response()
     resp.message(message)
+    resp.say(message)
+    #resp.say(message)
+    #if makeCall:
+    #    execfile("make_call.py " + getNumber())
 
     return str(resp)
 
 
-
+# Get these credentials from http://twilio.com/user/account
 if __name__ == "__main__":
     app.run(debug=True)
